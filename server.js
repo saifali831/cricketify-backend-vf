0ae4493ce698912dc9c91510b4f1eed12bb481c6
saifali831 <85249436+saifali831@users.noreply.github.com>
2021-06-23 20:36:00 +0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const {adminExist} = require('./utils/helpers');
 // mongodb connection
 //`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`
 //mongodb+srv://saifZafar:User0007@cluster0.3nkrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-db.mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`,{
+db.mongoose.connect(`mongodb+srv://saifZafar:User0007@cluster0.3nkrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majorit`,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
